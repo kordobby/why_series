67dd5d0abc9d9be7c35590d3397f29a3bebd58e4
Yoon Lee <poordobby@naver.com>
2023-05-11 23:19:56 +0900

    add. clouser

--- src/pages/whyClosure.ts
+++ src/pages/whyClosure.ts
@@ -1,9 +1,11 @@
 export const whyClosure = () => {
-  function outer(age: number) {
-    const name = "leetrue";
-    console.log(`hi, i'm ${name} and ${age} years old`);
+  function outer(country: string) {
+    let language = "한국어";
+    return function inner(name: string, age: number) {
+      console.log(`제 이름은 ${name} 입니다. 저는 ${age}살 입니다. 
+        저는 ${country}에 살고 ${language}를 사용해요.`);
+    };
   }
-
-  const whoAmI = () => outer(15);
-  whoAmI();
+  let a = outer("고양");
+  a("이트루", 22);
 };
